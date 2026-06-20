ba6368fab3a377035d24b792160f12fd02547240
Suraj Kumar <sv5872606@gmail.com>
2026-06-20 12:41:49 +0530

    fix db connection and add debug test

--- db.php
+++ db.php
@@ -11,23 +11,20 @@ $password = "IsLzggkvlMILATUboDzAAMPsIdndYsZY";
 try {
 
     $pdo = new PDO(
-    "mysql:host=thomas.proxy.rlwy.net;port=18165;dbname=railway;charset=utf8mb4",
-    "root",
-    "IsLzggkvlMILATUboDzAAMPsIdndYsZY",
-    [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-    ]
-);
-
-    $pdo->query("SELECT 1");
+        "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4",
+        $username,
+        $password,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]
+    );
 
     echo json_encode([
         "status" => true,
-        "message" => "DB connected"
+        "message" => "DB CONNECTED"
     ]);
 
-} catch (PDOException $e) {
+} catch (Exception $e) {
 
     echo json_encode([
         "status" => false,

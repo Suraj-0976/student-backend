7da36e79b411293cdd67618b37828760306903fc
Suraj Kumar <sv5872606@gmail.com>
2026-06-20 12:46:33 +0530

    all ok updated

--- add.php
+++ add.php
@@ -1,8 +1,42 @@
 <?php
 
-header("Content-Type: application/json");
+require_once 'db.php';
 
-echo json_encode([
-    "step" => "FILE HIT OK"
-]);
-exit;
+header('Content-Type: application/json');
+
+try {
+
+    $name  = trim($_POST['name'] ?? '');
+    $email = trim($_POST['email'] ?? '');
+
+    if(empty($name) || empty($email)){
+
+        echo json_encode([
+            "status" => false,
+            "message" => "All fields required"
+        ]);
+        exit;
+    }
+
+    $stmt = $pdo->prepare("
+        INSERT INTO students(name,email)
+        VALUES(:name,:email)
+    ");
+
+    $stmt->execute([
+        "name" => $name,
+        "email" => $email
+    ]);
+
+    echo json_encode([
+        "status" => true,
+        "message" => "Student Added"
+    ]);
+
+} catch(Exception $e){
+
+    echo json_encode([
+        "status" => false,
+        "message" => $e->getMessage()
+    ]);
+}
